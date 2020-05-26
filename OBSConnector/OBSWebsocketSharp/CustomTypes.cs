f61440b8237c6da43ece6729e2b1b67aece25299
Glax <gfreak70@gmail.com>
2020-05-26 15:48:36 +0200

    made SpecialSourceType more Accesible

--- OBSConnector/OBSWebsocketSharp/CustomTypes.cs
+++ OBSConnector/OBSWebsocketSharp/CustomTypes.cs
@@ -23,18 +23,18 @@ namespace OBSWebsocketSharp
         public string videoFormat, colorSpace, colorRange, scaleType;
     }
 
-    public enum sourceType { desktop1, desktop2, mic1, mic2, mic3 };
+    public enum _specialSourceType { desktop1, desktop2, mic1, mic2, mic3 };
     public struct SpecialSources
     {
-        public Dictionary<sourceType, string> sources;
+        public Dictionary<_specialSourceType, string> sources;
         public SpecialSources(string desktop1SourceName, string desktop2SourceName, string mic1SourceName, string mic2SourceName, string mic3SourceName)
         {
-            sources = new Dictionary<sourceType, string>();
-            sources.Add(sourceType.desktop1, desktop1SourceName);
-            sources.Add(sourceType.desktop2, desktop2SourceName);
-            sources.Add(sourceType.mic1, mic1SourceName);
-            sources.Add(sourceType.mic2, mic2SourceName);
-            sources.Add(sourceType.mic3, mic3SourceName);
+            sources = new Dictionary<_specialSourceType, string>();
+            sources.Add(_specialSourceType.desktop1, desktop1SourceName);
+            sources.Add(_specialSourceType.desktop2, desktop2SourceName);
+            sources.Add(_specialSourceType.mic1, mic1SourceName);
+            sources.Add(_specialSourceType.mic2, mic2SourceName);
+            sources.Add(_specialSourceType.mic3, mic3SourceName);
         }
     }
 
