d4ac9aed5bc810c8ee6449a53dfbb72cd15b7f2b
Glax <gfreak70@gmail.com>
2020-05-28 00:52:31 +0200

    Added StreamStatusEvent

--- OBSConnector/OBSWebsocketSharp/CustomTypes.cs
+++ OBSConnector/OBSWebsocketSharp/CustomTypes.cs
@@ -23,23 +23,27 @@ namespace OBSWebsocketSharp
         public string videoFormat, colorSpace, colorRange, scaleType;
     }
 
-    public enum _specialSourceType { desktop1, desktop2, mic1, mic2, mic3 };
+    public enum SpecialSourceType { desktop1, desktop2, mic1, mic2, mic3 };
     public struct SpecialSources
     {
-        public Dictionary<_specialSourceType, string> sources;
+        public Dictionary<SpecialSourceType, string> sources;
         public SpecialSources(string desktop1SourceName, string desktop2SourceName, string mic1SourceName, string mic2SourceName, string mic3SourceName)
         {
-            sources = new Dictionary<_specialSourceType, string>();
-            sources.Add(_specialSourceType.desktop1, desktop1SourceName);
-            sources.Add(_specialSourceType.desktop2, desktop2SourceName);
-            sources.Add(_specialSourceType.mic1, mic1SourceName);
-            sources.Add(_specialSourceType.mic2, mic2SourceName);
-            sources.Add(_specialSourceType.mic3, mic3SourceName);
+            sources = new Dictionary<SpecialSourceType, string>
+            {
+                { SpecialSourceType.desktop1, desktop1SourceName },
+                { SpecialSourceType.desktop2, desktop2SourceName },
+                { SpecialSourceType.mic1, mic1SourceName },
+                { SpecialSourceType.mic2, mic2SourceName },
+                { SpecialSourceType.mic3, mic3SourceName }
+            };
         }
     }
 
-    public struct StreamingStatus
+    public struct StreamStatus
     {
-        public bool streaming, recording, recordingPaused;
+        public bool streaming, recording, recordingPaused, replayBufferActive;
+        public int bytesPerSec, kbitsPerSec, totalStreamTime, numTotalFrames, numDroppedFrames, renderTotalFrames, renderMissedFrames, outputTotalFrames, outputSkippedFrames;
+        public double strain, fps, averageFrameTime, cpuUsage, memoryUsage, freeDiskSpace;
     }
 }
--- OBSConnector/OBSWebsocketSharp/Events.cs
+++ OBSConnector/OBSWebsocketSharp/Events.cs
@@ -1,14 +1,15 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
 
 namespace OBSWebsocketSharp
 {
     public partial class OBSConnector
     {
 
-        public event OBSSceneSwitchedEventHandler OnOBSSceneSwitched;
-        public delegate void OBSSceneSwitchedEventHandler(object sender, OBSSceneSwitchedEventArgs e);
-        public class OBSSceneSwitchedEventArgs : EventArgs
+        public event SceneSwitchedEventHandler OnSceneSwitched;
+        public delegate void SceneSwitchedEventHandler(object sender, SceneSwitchedEventArgs e);
+        public class SceneSwitchedEventArgs : EventArgs
         {
             public string newSceneName;
         }
@@ -16,69 +17,76 @@ namespace OBSWebsocketSharp
         {
         }
 
-        public event OBSStreamStartingEventHandler OnOBSStreamStarting;
-        public delegate void OBSStreamStartingEventHandler(object sender, OBSEventArgs e);
+        public event StreamStartingEventHandler OnStreamStarting;
+        public delegate void StreamStartingEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSStreamStartedEventHandler OnOBSStreamStarted;
-        public delegate void OBSStreamStartedEventHandler(object sender, OBSEventArgs e);
+        public event StreamStartedEventHandler OnStreamStarted;
+        public delegate void StreamStartedEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSStreamStoppingEventHandler OnOBSStreamStopping;
-        public delegate void OBSStreamStoppingEventHandler(object sender, OBSEventArgs e);
+        public event StreamStoppingEventHandler OnStreamStopping;
+        public delegate void StreamStoppingEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSStreamStoppedEventHandler OnOBSStreamStopped;
-        public delegate void OBSStreamStoppedEventHandler(object sender, OBSEventArgs e);
+        public event StreamStoppedEventHandler OnStreamStopped;
+        public delegate void StreamStoppedEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSRecordingStartingEventHandler OnOBSRecordingStarting;
-        public delegate void OBSRecordingStartingEventHandler(object sender, OBSEventArgs e);
+        public event RecordingStartingEventHandler OnRecordingStarting;
+        public delegate void RecordingStartingEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSRecordingStartedEventHandler OnOBSRecordingStarted;
-        public delegate void OBSRecordingStartedEventHandler(object sender, OBSEventArgs e);
+        public event RecordingStartedEventHandler OnRecordingStarted;
+        public delegate void RecordingStartedEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSRecordingStoppingEventHandler OnOBSRecordingStopping;
-        public delegate void OBSRecordingStoppingEventHandler(object sender, OBSEventArgs e);
+        public event RecordingStoppingEventHandler OnRecordingStopping;
+        public delegate void RecordingStoppingEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSRecordingStoppedEventHandler OnOBSRecordingStopped;
-        public delegate void OBSRecordingStoppedEventHandler(object sender, OBSEventArgs e);
+        public event RecordingStoppedEventHandler OnRecordingStopped;
+        public delegate void RecordingStoppedEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSRecordingPausedEventHandler OnOBSRecordingPaused;
-        public delegate void OBSRecordingPausedEventHandler(object sender, OBSEventArgs e);
+        public event RecordingPausedEventHandler OnRecordingPaused;
+        public delegate void RecordingPausedEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSRecordingResumedEventHandler OnOBSRecordingResumed;
-        public delegate void OBSRecordingResumedEventHandler(object sender, OBSEventArgs e);
+        public event RecordingResumedEventHandler OnRecordingResumed;
+        public delegate void RecordingResumedEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSReplayStartingEventHandler OnOBSReplayStarting;
-        public delegate void OBSReplayStartingEventHandler(object sender, OBSEventArgs e);
+        public event ReplayStartingEventHandler OnReplayStarting;
+        public delegate void ReplayStartingEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSReplayStartedEventHandler OnOBSReplayStarted;
-        public delegate void OBSReplayStartedEventHandler(object sender, OBSEventArgs e);
+        public event ReplayStartedEventHandler OnReplayStarted;
+        public delegate void ReplayStartedEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSReplayStoppingEventHandler OnOBSReplayStopping;
-        public delegate void OBSReplayStoppingEventHandler(object sender, OBSEventArgs e);
+        public event ReplayStoppingEventHandler OnReplayStopping;
+        public delegate void ReplayStoppingEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSReplayStoppedEventHandler OnOBSReplayStopped;
-        public delegate void OBSReplayStoppedEventHandler(object sender, OBSEventArgs e);
+        public event ReplayStoppedEventHandler OnReplayStopped;
+        public delegate void ReplayStoppedEventHandler(object sender, OBSEventArgs e);
 
-        public event OBSSourceVolumeChangedEventHandler OnOBSSourceVolumeChanged;
-        public delegate void OBSSourceVolumeChangedEventHandler(object sender, OBSSourceVolumeChangedEventArgs e);
-        public class OBSSourceVolumeChangedEventArgs : EventArgs
+        public event SourceVolumeChangedEventHandler OnSourceVolumeChanged;
+        public delegate void SourceVolumeChangedEventHandler(object sender, SourceVolumeChangedEventArgs e);
+        public class SourceVolumeChangedEventArgs : EventArgs
         {
             public string sourcename;
             public float sourcevolume;
         }
 
-        public event OBSSourceMuteStateChangedEventHandler OnOBSSourceMuteStateChanged;
-        public delegate void OBSSourceMuteStateChangedEventHandler(object sender, OBSSourceMuteStateChangedEventArgs e);
-        public class OBSSourceMuteStateChangedEventArgs : EventArgs
+        public event SourceMuteChangedEventHandler OnSourceMuteChanged;
+        public delegate void SourceMuteChangedEventHandler(object sender, SourceMuteChangedEventArgs e);
+        public class SourceMuteChangedEventArgs : EventArgs
         {
             public string sourcename;
             public bool muted;
         }
 
-        public event OBSSourceSourceRenamedEventHandler OnOBSSourceSourceRenamed;
-        public delegate void OBSSourceSourceRenamedEventHandler(object sender, OBSSourceSourceRenamedEventArgs e);
-        public class OBSSourceSourceRenamedEventArgs : EventArgs
+        public event SourceRenamedEventHandler OnSourceRenamed;
+        public delegate void SourceRenamedEventHandler(object sender, SourceRenamedEventArgs e);
+        public class SourceRenamedEventArgs : EventArgs
         {
             public string previousName, newName;
+        }
+
+        public event StreamStatusUpdateEventHandler OnStreamStatusUpdate;
+        public delegate void StreamStatusUpdateEventHandler(object sender, StreamStatusUpdateEventArgs e);
+        public class StreamStatusUpdateEventArgs : EventArgs
+        {
+            public StreamStatus status;
         }
 
         protected virtual void RaiseEvent(JObject message)
@@ -86,72 +94,98 @@ namespace OBSWebsocketSharp
             switch ((string)message["update-type"])
             {
                 case "SwitchScenes":
-                    OnOBSSceneSwitched?.Invoke(this, new OBSSceneSwitchedEventArgs()
+                    OnSceneSwitched?.Invoke(this, new SceneSwitchedEventArgs()
                     {
                         newSceneName = (string)message["scene-name"]
                     });
                     break;
                 case "StreamStarting":
-                    OnOBSStreamStarting?.Invoke(this, new OBSEventArgs());
+                    OnStreamStarting?.Invoke(this, new OBSEventArgs());
                     break;
                 case "StreamStarted":
-                    OnOBSStreamStarted?.Invoke(this, new OBSEventArgs());
+                    OnStreamStarted?.Invoke(this, new OBSEventArgs());
                     break;
                 case "StreamStopping":
-                    OnOBSStreamStopping?.Invoke(this, new OBSEventArgs());
+                    OnStreamStopping?.Invoke(this, new OBSEventArgs());
                     break;
                 case "StreamStopped":
-                    OnOBSStreamStopped?.Invoke(this, new OBSEventArgs());
+                    OnStreamStopped?.Invoke(this, new OBSEventArgs());
                     break;
                 case "RecordingStarting":
-                    OnOBSRecordingStarting?.Invoke(this, new OBSEventArgs());
+                    OnRecordingStarting?.Invoke(this, new OBSEventArgs());
                     break;
                 case "RecordingStarted":
-                    OnOBSRecordingStarted?.Invoke(this, new OBSEventArgs());
+                    OnRecordingStarted?.Invoke(this, new OBSEventArgs());
                     break;
                 case "RecordingStopping":
-                    OnOBSRecordingStopping?.Invoke(this, new OBSEventArgs());
+                    OnRecordingStopping?.Invoke(this, new OBSEventArgs());
                     break;
                 case "RecordingStopped":
-                    OnOBSRecordingStopped?.Invoke(this, new OBSEventArgs());
+                    OnRecordingStopped?.Invoke(this, new OBSEventArgs());
                     break;
                 case "RecordingPaused":
-                    OnOBSRecordingPaused?.Invoke(this, new OBSEventArgs());
+                    OnRecordingPaused?.Invoke(this, new OBSEventArgs());
                     break;
                 case "RecordingResumed":
-                    OnOBSRecordingResumed?.Invoke(this, new OBSEventArgs());
+                    OnRecordingResumed?.Invoke(this, new OBSEventArgs());
                     break;
                 case "ReplayStarting":
-                    OnOBSReplayStarting?.Invoke(this, new OBSEventArgs());
+                    OnReplayStarting?.Invoke(this, new OBSEventArgs());
                     break;
                 case "ReplayStarted":
-                    OnOBSReplayStarted?.Invoke(this, new OBSEventArgs());
+                    OnReplayStarted?.Invoke(this, new OBSEventArgs());
                     break;
                 case "ReplayStopping":
-                    OnOBSReplayStopping?.Invoke(this, new OBSEventArgs());
+                    OnReplayStopping?.Invoke(this, new OBSEventArgs());
                     break;
                 case "ReplayStopped":
-                    OnOBSReplayStopped?.Invoke(this, new OBSEventArgs());
+                    OnReplayStopped?.Invoke(this, new OBSEventArgs());
                     break;
                 case "SourceVolumeChanged":
-                    OnOBSSourceVolumeChanged?.Invoke(this, new OBSSourceVolumeChangedEventArgs()
+                    OnSourceVolumeChanged?.Invoke(this, new SourceVolumeChangedEventArgs()
                     {
                         sourcename = (string)message["sourceName"],
                         sourcevolume = Convert.ToSingle((string)message["volume"])
                     });
                     break;
                 case "SourceMuteStateChanged":
-                    OnOBSSourceMuteStateChanged?.Invoke(this, new OBSSourceMuteStateChangedEventArgs()
+                    OnSourceMuteChanged?.Invoke(this, new SourceMuteChangedEventArgs()
                     {
                         sourcename = (string)message["sourceName"],
                         muted = Convert.ToBoolean((string)message["muted"])
                     });
                     break;
                 case "SourceSourceRenamed":
-                    OnOBSSourceSourceRenamed?.Invoke(this, new OBSSourceSourceRenamedEventArgs()
+                    OnSourceRenamed?.Invoke(this, new SourceRenamedEventArgs()
                     {
                         previousName = (string)message["previousName"],
                         newName = (string)message["newName"]
+                    });
+                    break;
+                case "StreamStatus":
+                    OnStreamStatusUpdate?.Invoke(this, new StreamStatusUpdateEventArgs()
+                    {
+                        status = new StreamStatus()
+                        {
+                            recording = message["recording"].ToObject<bool>(),
+                            replayBufferActive = message["replay-buffer-active	"].ToObject<bool>(),
+                            streaming = message["streaming"].ToObject<bool>(),
+                            bytesPerSec = message["bytes-per-sec"].ToObject<int>(),
+                            kbitsPerSec = message["kbits-per-sec"].ToObject<int>(),
+                            totalStreamTime = message["total-stream-time"].ToObject<int>(),
+                            numTotalFrames = message["num-total-frames"].ToObject<int>(),
+                            numDroppedFrames = message["num-dropped-frames"].ToObject<int>(),
+                            renderTotalFrames = message["render-total-frames"].ToObject<int>(),
+                            renderMissedFrames = message["render-missed-frames"].ToObject<int>(),
+                            outputTotalFrames = message["output-total-frames"].ToObject<int>(),
+                            outputSkippedFrames = message["output-skipped-frames"].ToObject<int>(),
+                            strain = message["strain"].ToObject<double>(),
+                            fps = message["fps"].ToObject<double>(),
+                            averageFrameTime = message["average-frame-time"].ToObject<double>(),
+                            cpuUsage = message["cpu-usage"].ToObject<double>(),
+                            memoryUsage = message["memory-usage"].ToObject<double>(),
+                            freeDiskSpace = message["free-disk-space"].ToObject<double>()
+                        }
                     });
                     break;
                 default:
